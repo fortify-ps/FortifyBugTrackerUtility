5993833544cd41fe90fa8a6c3e617c12d1561a14
Ryan Black <ryanblack@gmail.com>
2016-08-09 07:44:57 -0500

    Revised credential error message and commented default arguments in the config file

--- fortify-util/rest-util/src/main/java/com/fortify/util/rest/RestConnection.java
+++ fortify-util/rest-util/src/main/java/com/fortify/util/rest/RestConnection.java
@@ -129,7 +129,7 @@ public class RestConnection implements IRestConnection {
 			return null;
 		} else {
 			String reasonPhrase = getReasonPhrase(response);
-			String msg = "Error accessing bug tracker: "+reasonPhrase;
+			String msg = reasonPhrase; //In the event of FoD credential exceptions the bug tracker reference was misleading - removed
 			String longMsg = msg+", response contents: \n"+response.getEntity(String.class);
 			// By adding a new exception as the cause, we make sure that the response
 			// contents will be logged whenever this RuntimeException is logged.
