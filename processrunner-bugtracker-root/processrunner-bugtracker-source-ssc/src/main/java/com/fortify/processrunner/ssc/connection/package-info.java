029b89dd668e6d43a9cf25a455ce48e15f17ba79
Ruud Senden <ruud.senden@hpe.com>
2017-06-01 23:48:44 +0200

    Initial attempt at adding support for filing bugs via SSC (probably not working yet)

--- processrunner-bugtracker-root/processrunner-bugtracker-source-ssc/src/main/java/com/fortify/processrunner/ssc/connection/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.fortify.processrunner.ssc.connection;
-/**
- * This package contains various classes and interfaces for managing SSC REST connections based on
- * process runner context properties (see {@link com.fortify.processrunner.context.Context} and
- * related classes and interfaces). 
- * 
- */
