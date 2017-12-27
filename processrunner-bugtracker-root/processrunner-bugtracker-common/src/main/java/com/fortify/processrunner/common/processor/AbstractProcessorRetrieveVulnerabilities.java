5ff56c59ce00360653f6988e1b6023d4e298c763
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2017-12-27 15:58:56 +0100

    Updated copyright statement for all Java source files and LICENSE.TXT files

--- processrunner-bugtracker-root/processrunner-bugtracker-common/src/main/java/com/fortify/processrunner/common/processor/AbstractProcessorRetrieveVulnerabilities.java
+++ processrunner-bugtracker-root/processrunner-bugtracker-common/src/main/java/com/fortify/processrunner/common/processor/AbstractProcessorRetrieveVulnerabilities.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * (c) Copyright 2017 EntIT Software LLC
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a 
  * copy of this software and associated documentation files (the 
  * "Software"), to deal in the Software without restriction, including without 
